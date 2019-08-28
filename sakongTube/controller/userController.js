8b16137cf47d9cbfbdf29ea2ee4e1f225a1a40e0
sgjh4040 <sgjh4040@naver.com>
2019-08-28 18:02:55 +0900

    -레이아웃 구성

---
+++ sakongTube/controller/userController.js
@@ -0,0 +1,7 @@
+export const join = (req,res)=>res.send("JOIN");
+export const login = (req,res)=>res.send("LOGIN");
+export const logout = (req,res)=>res.send("LOGOUT");
+export const users = (req,res)=>res.send("users");
+export const userDetail = (req,res)=>res.send("userDetail");
+export const editProfile = (req,res)=>res.send("editProfile");
+export const changePassword = (req,res)=>res.send("changePassword");
--- sakongTube/routers/userRouter.js
+++ sakongTube/routers/userRouter.js
@@ -1,9 +1,12 @@
 import express from "express";
+import routes from "../routes";
+import { users, userDetail, editProfile, changePassword } from "../controller/userController";
 
 const userRouter = express.Router();
 
-userRouter.get('/',(req,res)=>res.send('userRouter'));
-userRouter.get('/edit',(req,res)=>res.send('user edit'))
-userRouter.get('/password',(req,res)=>res.send('user password'))
+userRouter.get(routes.users,users);
+userRouter.get(routes.userDetail,userDetail);
+userRouter.get(routes.editProfile,editProfile);
+userRouter.get(routes.changePassword,changePassword);
 
 export default userRouter
